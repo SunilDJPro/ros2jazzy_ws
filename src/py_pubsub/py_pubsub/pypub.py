644644e38b92dba859ad7b70084e16a169da9456
SunilDJPro <tce.sunilprasath@gmail.com>
2024-09-15 17:13:48 +0530

    py_pub_init

---
+++ src/py_pubsub/py_pubsub/pypub.py
@@ -0,0 +1,35 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('simple_pub_py')
+        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        timer_period = 1  #seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = 'I am working for %d' % self.i + ' seconds!'
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+
+    rclpy.spin(minimal_publisher)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
